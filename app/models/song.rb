ab2e4b61445cf26fb5ec71e2b5de1318d94abcaf
grandballoon <luke.sovern.conley@gmail.com>
2018-07-25 14:27:20 -0400

    fixed notes feature

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,5 +2,30 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   has_many :notes
-end
 
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+    self.save
+  end
+
+  def artist_name
+    self.artist ? self.artist.name : nil
+  end
+
+  def note_contents=(notes)
+    notes.each do |note_content|
+      note = Note.create(content: note_content)
+      self.notes << note
+      self.save
+    end
+  end
+
+  def note_contents
+    self.notes.map do |note|
+      if note.content.strip != "" && note.content != nil
+      note.content
+      end
+    end
+  end
+
+end
